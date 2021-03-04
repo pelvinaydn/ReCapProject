065a64bed2c0da368872f5e82bde09cd47752aff
pelvinaydn <pelvin16@gmail.com>
2021-03-04 21:34:27 +0300

    API Image operations

---
+++ Business/Concrete/CarImageManager.cs
@@ -0,0 +1,62 @@
+﻿using Business.Abstract;
+using Business.Constants;
+using Core.Utilities.Helpers;
+using Core.Utilities.Results;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class CarImageManager : ICarImageService
+    {
+        ICarImageDal _carImageDal;
+
+        public CarImageManager(ICarImageDal carImageDal)
+        {
+            _carImageDal = carImageDal;
+        }
+
+        public IResult Add(IFormFile file, CarImage carImage)
+        {
+            carImage.ImagePath = FileHelper.Add(file);
+            carImage.DateTime = DateTime.Now;
+
+            _carImageDal.Add(carImage);
+            return new SuccessResult();
+        }
+
+        public IResult Delete(CarImage carImage)
+        {
+            FileHelper.Delete(carImage.ImagePath);
+            _carImageDal.Delete(carImage);
+            return new SuccessResult(Messages.Deleted);
+        }
+
+        public IDataResult<List<CarImage>> GetAll()
+        {
+            return new SuccessDataResult<List<CarImage>>(_carImageDal.GetAll());
+        }
+
+        public IDataResult<CarImage> GetById(int id)
+        {
+            return new SuccessDataResult<CarImage>(_carImageDal.Get(c => c.ImageId == id));
+        }
+
+        public IDataResult<CarImage> GetCarById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IResult Update(IFormFile file, CarImage carImage)
+        {
+            carImage.ImagePath = FileHelper.Update(_carImageDal.Get(c => c.ImageId == carImage.ImageId).ImagePath, file);
+            carImage.DateTime = DateTime.Now;
+            _carImageDal.Update(carImage);
+            return new SuccessResult(Messages.Updated);
+        }
+    }
+}
